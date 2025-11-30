a8914c9b4c24df4ac9df162328b0f5fb6f1c6fac
Ziming Lei <ziminglei123@gmail.com>
2025-11-30 12:09:23 -0800

    changed profile icon style

--- src/frontend/components/Profile.jsx
+++ src/frontend/components/Profile.jsx
@@ -1,13 +1,28 @@
-import images from '../images/user_profile_icon/default_user.png'
+import defaultImage from '../images/user_profile_icon/default_user.png';
 
-export default function Profile() {
+export default function Profile({image = defaultImage, color = '#add8e6'}) {
     return (
-        <img
-            src={images}
-            alt="Profile Picture"
+        <div
             style={{
-                width: "50px", height: "50px", padding: "10px"
+                width: "50px",
+                height: "50px",
+                backgroundColor: color,
+                borderRadius: "50%",
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+                overflow: "hidden",
+                border: "2px solid black"
             }}
-        />
+        >
+            <img
+                src={image}
+                alt="Profile Picture"
+                style={{
+                    width: "80%",
+                    height: "80%"
+                }}
+            />
+        </div>
     );
 }
