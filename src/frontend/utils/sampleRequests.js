b248decd6e79204f246cead21c088f008d180f50
Ziming Lei <ziminglei123@gmail.com>
2025-12-09 22:13:45 -0800

    factor the sample requests to a separate file

---
+++ src/frontend/utils/sampleRequests.js
@@ -0,0 +1,104 @@
+export const sampleRequests = [
+    {
+        request_id: 1,
+        student_icon: "../images/user_profile_icon/woman_5.png",
+        student_icon_bg_color: "#b8a6d4ff",
+        student_name: "Jackie May",
+        request_text: "I want to drink water",
+        created_at: "2025-12-07 01:41:32.058925+00",
+        status: "pending",
+        responded_at: null,
+        message: null,
+    },
+    {
+        request_id: 2,
+        student_icon: "../images/user_profile_icon/woman_2.png",
+        student_icon_bg_color: "#ffb6c1",
+        student_name: "Jackie Chan",
+        request_text: "I feel sick",
+        created_at: "2025-11-28 00:03:50.03132+00",
+        status: "approved",
+        responded_at: "2025-11-28 02:41:32.058925+00",
+        message: null,
+    },
+    {
+        request_id: 3,
+        student_icon: "../images/user_profile_icon/polar_bear_1.png",
+        student_icon_bg_color: "#b0ecacff",
+        student_name: "Sarah Doe",
+        request_text: "I want to drink water",
+        created_at: "2025-11-24 00:03:50.03132+00",
+        status: "message",
+        responded_at: null,
+        message: {
+            message_id: 12,
+            content:
+            "Please return the signed form by tomorrow. aaas sa an asdm asdnasd sak. sd ak d aks da kd qwk dandedkeja dkfea nf ekn dkwedknend ekn ksad adsaed,em cde dejd kedn a,rnf r efea jf jh. ",
+            sent_at: "2025-11-24 00:03:50.03132+00",
+        },
+    },
+    {
+        request_id: 4,
+        student_icon: "../images/user_profile_icon/polar_bear_1.png",
+        student_icon_bg_color: "#b0ecacff",
+        student_name: "Sarah Doe",
+        request_text: "I want to drink water",
+        created_at: "2025-11-24 00:03:50.03132+00",
+        status: "message",
+        responded_at: null,
+        message: {
+            message_id: 12,
+            content:
+            "Please return the signed form by tomorrow. aaas sa an asdm asdnasd sak. sd ak d aks da kd qwk dandedkeja dkfea nf ekn dkwedknend ekn ksad adsaed,em cde dejd kedn a,rnf r efea jf jh. ",
+            sent_at: "2025-11-24 00:03:50.03132+00",
+        },
+    },
+    {
+        request_id: 5,
+        student_icon: "../images/user_profile_icon/polar_bear_1.png",
+        student_icon_bg_color: "#b0ecacff",
+        student_name: "Sarah Doe",
+        request_text: "I feel sick",
+        created_at: "2025-11-22 00:03:50.03132+00",
+        status: "message",
+        responded_at: null,
+        message: {
+            message_id: 12,
+            content:
+            "Please return the signed form by tomorrow. aaas sa an asdm asdnasd sak. sd ak d aks da kd qwk dandedkeja dkfea nf ekn dkwedknend ekn ksad adsaed,em cde dejd kedn a,rnf r efea jf jh. ",
+            sent_at: "2025-11-22 00:03:50.03132+00",
+        },
+    },
+    {
+        request_id: 6,
+        student_icon: "../images/user_profile_icon/polar_bear_1.png",
+        student_icon_bg_color: "#b0ecacff",
+        student_name: "Sarah Doe",
+        request_text: "I need help",
+        created_at: "2025-11-21 00:03:50.03132+00",
+        status: "message",
+        responded_at: null,
+        message: {
+            message_id: 12,
+            content:
+            "Please return the signed form by tomorrow. aaas sa an asdm asdnasd sak. sd ak d aks da kd qwk dandedkeja dkfea nf ekn dkwedknend ekn ksad adsaed,em cde dejd kedn a,rnf r efea jf jh. ",
+            sent_at: "2025-11-21 00:03:50.03132+00",
+        },
+    },
+    {
+        request_id: 7,
+        student_icon: "../images/user_profile_icon/polar_bear_1.png",
+        student_icon_bg_color: "#b0ecacff",
+        student_name: "Sarah Doe",
+        request_text: "I want to go to the bathroom",
+        created_at: "2025-11-20 00:03:50.03132+00",
+        status: "message",
+        responded_at: null,
+        message: {
+            message_id: 12,
+            content:
+            "Please return the signed form by tomorrow. aaas sa an asdm asdnasd sak. sd ak d aks da kd qwk dandedkeja dkfea nf ekn dkwedknend ekn ksad adsaed,em cde dejd kedn a,rnf r efea jf jh. ",
+            sent_at: "2025-11-20 00:03:50.03132+00",
+        },
+    }
+];
